02c6dd31a994b746abff2b6a985217883ad02f3f
Shantha Kumara <wgskumara@gmail.com>
2024-10-25 19:35:34 +0530

    update ansible playbooks

--- bash-scripts/tomcat-start.sh
+++ bash-scripts/tomcat-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh ec2-user@3.88.17.233 sudo sh /opt/apache-tomcat-10.1.31/bin/startup.sh
+ssh -o StrictHostKeyChecking=no ec2-user@3.88.17.233 sudo sh /opt/apache-tomcat-10.1.31/bin/startup.sh
